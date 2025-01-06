8aae1d22b7c0c3f6e40d118702b25b3f32215e6c
k1llerpc <122518992+k1llerpc@users.noreply.github.com>
2025-01-06 11:57:51 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,9 +2,9 @@
 
 # Function to update the repository
 update_repository() {
-    echo "Updating repository..."
+    echo "UPDATE repository..."
     bash update.sh
-    echo "Repository updated successfully!"
+    echo "Repository UPDATE fertig!"
 }
 
 # Main script
@@ -12,7 +12,7 @@ clear
 echo
 pkg install pv -y >/dev/null 2>&1
 echo -e "\033[32m\033[1m{───────────────────────────────────────────────────}"
-echo -e "\033[33m\033[1m   Installing All Required Packages! Please Wait..." | pv -qL 10
+echo -e "\033[33m\033[1m   Installiere erforderliche Pakete..." | pv -qL 10
 apt update                    
 apt upgrade -y 
 pkg install python -y 
@@ -26,20 +26,20 @@ pip install random
 pip install requests 
 pkg install python2 -y 
 pkg install termux-api -y 
-echo -e "\033[31m\033[1m        INSTALLATION COMPLETED \033[32m[\033[36m✓\033[32m]" | pv -qL 12
+echo -e "\033[31m\033[1m        INSTALLATION ABGESCHLOSSEN \033[32m[\033[36m✓\033[32m]" | pv -qL 12
 echo -e "\033[33m\033[1m]────────────────────────────────────────────["
 termux-setup-storage
-cd $HOME/TermuX-Custom
+cd $HOME/MIB-TERMUX-STYLE
 cp login.sh $PREFIX/etc
 chmod +x login.sh delete.sh setup.sh sound_effect.py banner.sh update.sh
-mkdir -p $HOME/TermuX-Custom/Song 
-mv Access-Granted.mp3 Jarvis2.mp3 JARVIS.mp3 sound_effect.py $HOME/TermuX-Custom/Song
+mkdir -p $HOME/MIB-TERMUX-STYLE/Song 
+mv Access-Granted.mp3 Jarvis2.mp3 JARVIS.mp3 sound_effect.py $HOME/MIB-TERMUX-STYLE/Song
 rm 1
-mkdir -p $HOME/TermuX-Custom/NETWORK
-mv network.py $HOME/TermuX-Custom/NETWORK
+mkdir -p $HOME/MIB-TERMUX-STYLE/NETWORK
+mv network.py $HOME/MIB-TERMUX-STYLE/NETWORK
 
 # Prompt user for update
-read -p "Do you want to update the repository now? (y/n): " choice
+read -p "UPDATE DER repository JETZT? (y/n): " choice
 if [ "$choice" = "y" ]; then
     update_repository
 fi
